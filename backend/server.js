052b1a8300f0195078b52da80e5651ff097a5e47
eakamjot10@gmail.com <eakamjot10@gmail.com>
2026-01-14 15:01:47 +0530

    add code

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://team-inevitables-classroom-attentio.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
   }
 });
@@ -46,7 +46,7 @@ io.on("connection", (socket) => {
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_ORIGIN || "http://localhost:3000",
+    origin:"https://team-inevitables-classroom-attentio.vercel.app",
     credentials: true
   })
 );
